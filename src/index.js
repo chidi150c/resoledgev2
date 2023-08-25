488d8aef349ed6f8bafaffa051af54749a75f087
chidi150c <chidi150c@gmail.com>
2023-08-25 12:16:48 +0100

    remove redux

--- src/index.js
+++ src/index.js
@@ -5,23 +5,16 @@ import AppRouter from './MyComponents/AppRouter';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { Provider } from 'react-redux'; // Import the Provider component
-import store from './MyComponents/reduxStore'; // Correct the import path
 
-
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <AppRouter />
-    </Provider>,
+    <AppRouter />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
